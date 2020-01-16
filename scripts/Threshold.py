56b6f93cd7b33c6f55f309c7c0557acbbce16bd5
Dominik Lindner <dominikl@users.noreply.github.com>
2020-01-16 09:26:02 +0000

    Add comment

--- scripts/Threshold.py
+++ scripts/Threshold.py
@@ -21,6 +21,9 @@ def threshold_plane(pixels, threshold=None):
     """
 
     if threshold is None:
+        # perform simple automatic thresholding, see
+        # https://en.wikipedia.org/wiki/Thresholding_(image_processing)\
+        # #Automatic_thresholding 
         threshold = np.mean(pixels)
         min_value = np.amin(pixels)
         max_value = np.amax(pixels)
